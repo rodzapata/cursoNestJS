7947b70885370e7e565a96809ceba0ce8751421a
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2026-01-10 08:57:25 -0500

    create prueba module manually and udpdate module definition

--- api-nest/src/app.module.ts
+++ api-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './tasks/tasks.module';
+import { PruebaModule } from './prueba/prueba.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [TaskModule,PruebaModule],
   controllers: [AppController],
   providers: [AppService],
 })
